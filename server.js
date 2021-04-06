eb544568e5e20671f44b60032fb7a97e94cb337f
Fikayo <fikfikky@gmail.com>
2021-04-06 20:49:01 +0100

    Project complete

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+let bodyParser = require("body-parser");
 
 let port = process.env.PORT || "5000";
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname+'/index.html'));
 });
 
+app.post("/sendinfo", (req, res) => {
+
+    const email = req.body.email;
+    
+    res.send({email});
+});
+
 
 app.listen(port, () => {
     console.log(`App Running at http://localhost:${port}`);
 })
+
