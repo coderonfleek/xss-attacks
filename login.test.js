8623e439627247a1350bfd89e8ffc5d1764b7c2d
Fikayo <fikfikky@gmail.com>
2021-04-07 18:44:51 +0100

    Tests running

--- login.test.js
+++ login.test.js
@@ -4,7 +4,21 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
   let driver = await new Builder().forBrowser('chrome').build();
   try {
     await driver.get('http://localhost:5000');
-    /* await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+    await driver.findElement(By.id('userEmail')).sendKeys('fik4christ@yahoo.com');
+    await driver.findElement(By.id('userPassword')).sendKeys('fikayo');
+
+    
+
+    //Submit
+    await driver.findElement(By.id('submitButton')).click();
+
+    const emailText = await driver.wait(driver.findElement(By.id('infoDisplay')).getText(), 1000);
+
+    console.log(emailText);
+
+    //await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+    /* 
+    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
     await driver.wait(until.titleIs('webdriver - Google Search'), 1000); */
   } finally {
     await driver.quit();
