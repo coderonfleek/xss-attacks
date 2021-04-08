c137b7fb79200d735c0185a9fdc6fbd4f2cc2e7d
Fikayo <fikfikky@gmail.com>
2021-04-08 07:32:28 +0100

    Switched to try...finally

--- login.test.js
+++ login.test.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 
-let browser;
+/* let browser;
 
 beforeEach(async () => {
   browser = await puppeteer.launch();
@@ -8,21 +8,28 @@ beforeEach(async () => {
 
 afterEach(async () => {
   await browser.close();
-});
+}); */
 
 test("Check for XSS attack on email field", async () => {
-  const page = await browser.newPage();
-  
-  await page.goto('http://localhost:5000');
 
-  await page.type('#userEmail', '<input type="file" />');
-  await page.type('#userPassword', 'password');
-  await page.click('#submitButton');
-
-  let emailContainer = await page.$('#infoDisplay')
-  let value = await emailContainer.evaluate(el => el.textContent);
+  const browser = await puppeteer.launch();
+  try {
+    const page = await browser.newPage();
   
-  expect(value.length).toBeGreaterThan(0);
+    await page.goto('http://localhost:5000');
+
+    await page.type('#userEmail', '<input type="file" />');
+    await page.type('#userPassword', 'password');
+    await page.click('#submitButton');
+
+    let emailContainer = await page.$('#infoDisplay')
+    let value = await emailContainer.evaluate(el => el.textContent);
+    
+    expect(value.length).toBeGreaterThan(0);
+  } finally {
+    await browser.close();
+  }
+  
   
 }, 120000);
 
