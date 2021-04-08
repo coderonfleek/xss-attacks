2d5a95de9d8c68e8204427804ffb187ff7979ece
Fikayo <fikfikky@gmail.com>
2021-04-08 07:45:00 +0100

    Configuration file Added

--- login.test.js
+++ login.test.js
@@ -1,15 +1,5 @@
 const puppeteer = require('puppeteer');
 
-/* let browser;
-
-beforeEach(async () => {
-  browser = await puppeteer.launch();
-});
-
-afterEach(async () => {
-  await browser.close();
-}); */
-
 test("Check for XSS attack on email field", async () => {
 
   const browser = await puppeteer.launch();
@@ -32,27 +22,3 @@ test("Check for XSS attack on email field", async () => {
   
   
 }, 120000);
-
-/* const {Builder, By, Key, until} = require('selenium-webdriver');
-
-(async function example() {
-  let driver = await new Builder().forBrowser('chrome').build();
-  try {
-    await driver.get('http://localhost:5000');
-    await driver.findElement(By.id('userEmail')).sendKeys('fik4christ@yahoo.com');
-    await driver.findElement(By.id('userPassword')).sendKeys('fikayo');
-
-    
-
-    //Submit
-    await driver.findElement(By.id('submitButton')).click();
-
-    const emailText = await driver.findElement(By.id('infoDisplay')).getText();
-
-    console.log(emailText);
-    console.log(emailText.length);
-
-  } finally {
-    await driver.quit();
-  }
-})(); */
